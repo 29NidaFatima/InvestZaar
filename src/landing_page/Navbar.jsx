4b5a398e53588d8f7aa60df898e279b1bb1cb32d
NidaFatima <2529nida17@gmail.com>
2025-05-27 15:41:40 +0530

    .

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="sticky top-0 bg-white z-50 flex items-center justify-between px-6 lg:px-12 py-6 shadow-sm">
+      {/* Logo */}
+      <div className="flex items-center space-x-2">
+        <img
+          src="/assets/logo.svg" // Ensure this path is correct
+          alt="Zerodha Logo"
+          className="h-6"
+        />
+      </div>
+
+      {/* Navigation Links */}
+      <nav className="hidden md:flex items-center space-x-10 text-gray-600 text-base font-small">
+        <a href="#" className="hover:text-black transition">Signup</a>
+        <a href="#" className="hover:text-black transition">About</a>
+        <a href="#" className="hover:text-black transition">Products</a>
+        <a href="#" className="hover:text-black transition">Pricing</a>
+        <a href="#" className="hover:text-black transition">Support</a>
+      </nav>
+
+
+    </header>
+  );
+};
+
--- src/landing_page/home/HomePage.jsx
+++ src/landing_page/home/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Navbar } from '../Navbar'
 import { Hero } from './Hero'
 import { Awards } from './Awards'
-
 import { Pricing } from './Pricing'
 import { Education } from './Education'
 import { OpenAccount } from '../OpenAccount'
