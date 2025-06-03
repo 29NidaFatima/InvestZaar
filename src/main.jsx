8e4f2d8b0601a3859bc742f4a0316eb6a7d59ffe
NidaFatima <2529nida17@gmail.com>
2025-06-03 15:13:24 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './landing_page/Navbar';
 import HomePage from './landing_page/home/HomePage';
 import SignUp from './landing_page/signup/SignUp';
 import AboutPage from './landing_page/about/AboutPage';
-  import { ProductPage } from './landing_page/product/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage'
+import { ProductPage } from './landing_page/product/ProductPage';
 import SupportPage from './landing_page/support/SupportPage';
-import  PricingPage from './landing_page/pricing/PricingPage'
-import { Navbar } from './landing_page/Navbar';
 import { Footer } from './landing_page/Footer';
 
 const root = createRoot(document.getElementById('root'));
@@ -16,16 +16,16 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/pricing" element={<PricingPage />} /> 
+        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/support" element={<SupportPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
